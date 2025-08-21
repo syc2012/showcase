993ba848cd2588bd5e38f4b33c8e3f76d7a2632e
syc2012 <87954959+syc2012@users.noreply.github.com>
2025-08-21 20:19:39 +0800

    Update alignmemt.c

--- alignmemt.c
+++ alignmemt.c
@@ -2,64 +2,77 @@
 #include <inttypes.h>
 
 
-typedef struct _tData
+#define PACKED 0
+
+typedef struct _tAlign2
 {
-    unsigned int   A;
-    unsigned char  B;
-    unsigned char  C[6];
-    unsigned char  D[8];
-    unsigned short E;
-#if 0
-} tData;
+    unsigned char  A;
+    unsigned short B;
+    unsigned char  C;
+#if (PACKED)
+} __attribute__((packed)) tAlign2;
 #else
-} __attribute__((packed)) tData;
+} tAlign2;
 #endif
 
-unsigned char g_begin = 0x5a;
-tData g_data = {
-    .A = 588168,
-    .B = 128,
-    .C = { 0x1a, 0x1b, 0x1c, 0x00, 0x00, 0x01 },
-    .D = { 0x2a, 0x2b, 0x2c, 0x2d, 0x00, 0x00, 0x00, 0x02 },
-    .E = 0xcafe
+typedef struct _tAlign4
+{
+    unsigned char  A;
+    unsigned int   B;
+    unsigned char  C;
+#if (PACKED)
+} __attribute__((packed)) tAlign4;
+#else
+} tAlign4;
+#endif
+
+typedef struct _tAlign8
+{
+    unsigned char  A;
+    double         B;
+    unsigned char  C;
+#if (PACKED)
+} __attribute__((packed)) tAlign8;
+#else
+} tAlign8;
+#endif
+
+unsigned char g_begin = 0xff;
+tAlign2 g_align2 = {
+    .A = 0xaa,
+    .B = 16888,
+    .C = 0xcc
 };
-unsigned int g_end = 0xefbeadde;
+tAlign4 g_align4 = {
+    .A = 0xaa,
+    .B = 20250821,
+    .C = 0xcc
+};
+tAlign8 g_align8 = {
+    .A = 0xaa,
+    .B = 123.456789,
+    .C = 0xcc
+};
+unsigned char g_end = 0xff;
 
 int main(void)
 {
-    unsigned long long  val64;
-    unsigned char *byte;
-    int  i;
-
     printf("\n");
-    printf("&(g_begin)  = %p\n", &(g_begin));
-    printf("&(g_data)   = %p\n", &(g_data));
-    printf("&(g_data.A) = %p\n", &(g_data.A));
-    printf("&(g_data.B) = %p\n", &(g_data.B));
-    printf("&(g_data.C) = %p\n", &(g_data.C));
-    printf("&(g_data.D) = %p\n", &(g_data.D));
-    printf("&(g_data.E) = %p\n", &(g_data.E));
-    printf("&(g_end)    = %p\n", &(g_end));
-    printf("\n");
-
-    printf("g_data.A = 0x%x\n", g_data.A);
-    val64 = *((unsigned long long *)(g_data.D));
-    printf("*((unsigned long long *)(g_data.D)) = 0x%llx\n", val64);
-    printf("g_data.E = 0x%x\n", g_data.E);
-    printf("\n");
-
-    printf("sizeof( tData ) = %d\n", (int)sizeof( tData ));
-    printf("\n");
-
-    byte = (unsigned char *)&(g_begin);
-    for (i=0; i<48; i++)
-    {
-        if (((i % 8) == 0) && (i != 0)) printf("\n");
-        printf("%02X ", byte[i]);
-    }
-    printf("\n");
+    printf("&(g_begin)    = %p\n", &(g_begin));
+    printf("&(g_align2)   = %p (%lu bytes)\n", &(g_align2), sizeof(tAlign2));
+    printf("&(g_align2.A) = %p\n", &(g_align2.A));
+    printf("&(g_align2.B) = %p\n", &(g_align2.B));
+    printf("&(g_align2.C) = %p\n", &(g_align2.C));
+    printf("&(g_align4)   = %p (%lu bytes)\n", &(g_align4), sizeof(tAlign4));
+    printf("&(g_align4.A) = %p\n", &(g_align4.A));
+    printf("&(g_align4.B) = %p\n", &(g_align4.B));
+    printf("&(g_align4.C) = %p\n", &(g_align4.C));
+    printf("&(g_align8)   = %p (%lu bytes)\n", &(g_align8), sizeof(tAlign8));
+    printf("&(g_align8.A) = %p\n", &(g_align8.A));
+    printf("&(g_align8.B) = %p\n", &(g_align8.B));
+    printf("&(g_align8.C) = %p\n", &(g_align8.C));
+    printf("&(g_end)      = %p\n", &(g_end));
     printf("\n");
 
     return 0;
 }
-
